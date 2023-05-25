901f99f1be7310e0e7c8c5029f01288ea860a7db
ajibade-ibrahim <ibrahim.ajibade@trilogy.com>
2023-05-25 08:06:12 +0100

    Implemented basic CRUD operations

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common'
+import { TasksService } from './tasks.service'
+import { Task } from './task.model'
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(): Task[] {
+    return this.tasksService.getTasks()
+  }
+
+  @Get('/:id')
+  getTask(@Param('id') id: string): Task {
+    console.log('id: ', id)
+    const task = this.tasksService.getTask(Number(id))
+    return task == null ? null : task
+  }
+
+  @Post()
+  createTask(@Body() model: { title: string }) {
+    console.log('model: ', model)
+    if (model?.title?.length < 1) {
+      throw Error('Bad request')
+    }
+
+    return this.tasksService.putTask(model.title)
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: unknown) {
+    const taskId = Number(id)
+    if (isNaN(taskId)) {
+      throw Error('Bad request')
+    }
+    return this.tasksService.deleteTask(taskId)
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common'
+import { Task, TaskStatus } from './task.model'
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [
+    { id: 1, title: 'Create Models', status: TaskStatus.OPEN },
+    { id: 2, title: 'Create Controllers', status: TaskStatus.OPEN },
+    { id: 3, title: 'Create Database', status: TaskStatus.OPEN }
+  ]
+
+  getTasks() {
+    return this.tasks
+  }
+
+  putTask(title: string): Task {
+    const task = {
+      id: this.tasks.length + 1,
+      title,
+      status: TaskStatus.OPEN
+    }
+    this.tasks.push(task)
+    return task
+  }
+
+  getTask(id: number): Task | undefined {
+    console.log('service: id', id)
+    const task = this.tasks.find((task) => task.id === id)
+    console.log(task)
+    return task
+  }
+
+  deleteTask(id: number) {
+    console.log(id)
+    this.tasks.find((item, index, tasks) => {
+      if (item.id === id) {
+        tasks.splice(index, 1)
+        return true
+      }
+      return false
+    })
+  }
+}
