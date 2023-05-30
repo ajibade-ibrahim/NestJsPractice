529ece36882c6e0f7900baf82b5f5e5ee4549edf
ajibade-ibrahim <ibrahim.ajibade@trilogy.com>
2023-05-30 08:41:40 +0100

    Added empty space validator

--- src/tasks/dto/task-creation-dto.ts
+++ src/tasks/dto/task-creation-dto.ts
@@ -1,6 +1,8 @@
 import { IsNotEmpty } from 'class-validator'
+import { Transform, TransformFnParams } from 'class-transformer'
 
 export class TaskCreationDto {
   @IsNotEmpty()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   title!: string
 }
